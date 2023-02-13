1a2ab31022ff4121ae802ed248512db652bc5e47
Deimar <deimar.parn@voco.ee>
2023-02-13 18:40:22 +0000

    Created DB connection

---
+++ models/config.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+
+const config = {
+    db: {
+        host: process.env.host,
+        user: process.env.user,
+        password: process.env.password,
+        database: process.env.database
+    }
+}
+
+module.exports = config;
---
+++ models/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2/promise');
+const dbConfig = require('./config');
+
+async function query(sql, params) {
+    const connection = await mysql.createConnection(dbConfig.db);
+    const [ result ] = await connection.execute(sql, params);
+
+    return result;
+}
+
+module.exports = {
+    query
+}
