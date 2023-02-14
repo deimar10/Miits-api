5cc95c2ee2318ca41c8f5247da26f54d044f4f91
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-02-15 00:04:14 +0300

    Created enterprise login endpoint

--- routes/enterprise/enterprise.js
+++ routes/enterprise/enterprise.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const enterpriseController = require('../../controllers/enterprise/enterprise.js');
 
 router.post('/enterprise/register', enterpriseController.register);
+router.post('/enterprise/login', enterpriseController.login);
 
 module.exports = router;
