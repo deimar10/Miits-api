115feda753885f6cf052b4790325268603bf3825
Deimar <deimar.parn@voco.ee>
2023-02-19 20:30:07 +0000

    Created user create feedback endpoint

--- controllers/user/user.js
+++ controllers/user/user.js
@@ -27,3 +27,27 @@ async function assignFeedbackToOffer(offers) {
     }
     return offers;
 }
+
+exports.createUserFeedback = async (req, res) => {
+    try {
+        const offerTitle = req.params.title;
+        const { name, comment } = req.body;
+
+        const offerId = await db.query('SELECT pakkumised_id FROM pakkumised WHERE title = ?', [offerTitle]);
+
+        const result = await db.query('INSERT INTO tagasiside ( `name`, `comment`, `pakkumised_fk`) VALUES (?,?,?)',
+            [name, comment, offerId[0].pakkumised_id]);
+
+        if(result.affectedRows) {
+            return res.status(201).json({
+                tagasiside_id: result.insertId,
+                name: name,
+                comment: comment
+            });
+        }
+
+    } catch (error) {
+      console.log(`Error trying to create user feedback: ${error}`);
+      return res.status(400).send();
+    }
+}
--- routes/user/user.js
+++ routes/user/user.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const userController = require('../../controllers/user/user.js');
 
 router.get('/user/offers', userController.getOffers);
+router.post('/user/feedback/:title', userController.createUserFeedback);
 
 module.exports = router;
