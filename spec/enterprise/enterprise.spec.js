555c4b0da8a28120bb07550d9794cda50fb3f38a
Jürgen Mikk <jurgen.mikk@khk.ee>
2023-02-21 23:10:21 +0300

    Added enterprise endpoint unit testing & modified file structure

---
+++ spec/enterprise/enterprise.spec.js
@@ -0,0 +1,159 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../app.js');
+
+const should = chai.should();
+chai.use(chaiHttp);
+chai.use(require('chai-things'));
+chai.use(require('chai-like'));
+
+let prefix = '/miits/api';
+let offerId;
+describe('Testing enterprise endpoints', () => {
+
+    it('should successfully register an enterprise', (done) => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/register`)
+            .send({ username: 'KolmTilli', password: 'Lollakas1!'})
+            .end((err, res) => {
+                res.should.have.status(200);
+                done();
+        });
+    });
+
+    it('should throw empty input error during registration', () => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/register`)
+            .send( {username: '', password: ''})
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.error.message.should.be.a('string', 'Error: Empty user input error');
+        });
+    });
+
+    it('should throw name already taken error during registration', () => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/register`)
+            .send( { username: 'Shooters', password: 'Madistame123!' })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.error.message.should.be.a('string', 'Error: Name already taken');
+            });
+    });
+
+    it('should throw password validation error during', () => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/register`)
+            .send( { username: 'Up', password: 'Lollakas' })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.error.message.should.be.a('string', 'Error: Password validatio');
+            });
+    });
+
+    it('should successfully .auth enterprise', (done) => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/login`)
+            .send({ username: 'KolmTilli', password: 'Lollakas1!' })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('auth', true);
+                done();
+            });
+    });
+
+    it('should fail to .auth enterprise', () => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/login`)
+            .send({ username: 'KolmTilli', password: 'Lollak' })
+            .end((err, res) => {
+                res.should.have.status(401);
+                res.body.should.be.an('object');
+                res.body.should.have.property('auth', false);
+            });
+    });
+
+    it('should successfully get all enterprise specific offers', () => {
+        chai.request(server)
+            .get(`${prefix}/enterprise/offers/?enterprise=Shooters`)
+            .end((err, res) => {
+               res.should.have.status(200);
+               res.body.should.be.an('array').lengthOf(3);
+            });
+    });
+
+    it('should successfully create a new offer', (done) => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/offer/create`)
+            .send({
+                upcoming: false,
+                favorite: false,
+                enterprise: 'KolmTilli',
+                title: 'UusAlgus',
+                category: 'Event',
+                location: 'Tallinn',
+                date: '06.20.2023',
+                price: 49.99,
+                image: 'image/event/uusAlgus.jpg',
+                description: 'Vinge aastavahetuse pidu!'
+            })
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.be.an('object');
+                res.body.should.have.a.property('pakkumised_id');
+                res.body.should.have.a.property('title', 'UusAlgus');
+                res.body.should.have.a.property('enterprise', 'KolmTilli');
+                res.body.should.have.a.property('price', 49.99);
+                offerId = res.body.pakkumised_id;
+                done();
+            });
+    });
+
+    it('should fail to create a new offer', (done) => {
+        chai.request(server)
+            .post(`${prefix}/enterprise/offer/create`)
+            .send({
+                upcoming: false,
+                favorite: false,
+                enterprise: 'KolmTilli',
+                title: 'UusAlgus',
+                category: 'Event',
+                location: 'Tallinn',
+                date: '06/20/2023',
+                price: 49.99,
+                image: 'image/event/uusAlgus.jpg',
+                description: 'Vinge aastavahetuse pidu!'
+            })
+            .end((err, res) => {
+                res.should.have.status(400);
+                res.error.message.should.be.a('string', 'Error: Incorrect date format');
+                done();
+            });
+    });
+
+    it('should successfully edit an enterprise offer', () => {
+        chai.request(server)
+            .put(`${prefix}/enterprise/offer/edit/${offerId}`)
+            .send({
+                title: 'MetsikMõll',
+                location: 'Tallinn',
+                date: '06.20.2023',
+                price: 49.99,
+                description: 'Vinge aastavahetuse pidu!',
+                category: 'Event'
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+            });
+    });
+
+    it('should successfully delete an enterprise offer', () => {
+        chai.request(server)
+            .delete(`${prefix}/enterprise/offer/delete/${offerId}`)
+            .end((err, res) => {
+                res.should.have.status(204);
+            });
+    });
+
+});
---
+++ spec/user/user.spec.js
@@ -0,0 +1,64 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../../app.js');
+
+const should = chai.should();
+chai.use(chaiHttp);
+chai.use(require('chai-things'));
+chai.use(require('chai-like'));
+
+let prefix = '/miits/api';
+describe('Testing user endpoints', () => {
+
+    it('should get all offers from pakkumised table', () => {
+        chai.request(server)
+            .get(`${prefix}/user/offers`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('array');
+                res.body.should.all.have.property('pakkumised_id');
+                res.body.should.all.have.property('feedback');
+                res.body.should.all.have.property('title');
+                res.body.should.deep.include({
+                    pakkumised_id: 48,
+                    favorite: false,
+                    upcoming: false,
+                    enterprise: 'Level',
+                    title: 'Mimmer',
+                    category: 'Drinks',
+                    slug: 'Mimmer',
+                    location: 'Tartu',
+                    date: '06.20.2022',
+                    price: 55,
+                    image: 'image/url/litter.jpg',
+                    description: 'heaviest, richest drink in town!',
+                    reg_konto_fk: 1,
+                    feedback: [{ tagasiside_id: 17, name: 'Allu', comment: 'asdasdasdasd', pakkumised_fk: 48}]
+                });
+            });
+    });
+
+    it('should get a single offer', () => {
+        chai.request(server)
+            .get(`${prefix}/user/offers/offer-details/Mimmer`)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.an('object');
+                res.body.should.have.property('pakkumised_id', 48);
+                res.body.should.have.property('title', 'Mimmer');
+                res.body.should.have.property('reg_konto_fk', 1);
+            });
+    });
+
+    it('should create a new offer specific feedback in the tagasiside table', () => {
+        chai.request(server)
+            .post(`${prefix}/user/feedback/Strimmer`)
+            .send({ name: 'Martin', comment: 'Mega pidu!'})
+            .end((err, res) => {
+                res.should.have.status(201);
+                res.body.should.have.property('tagasiside_id');
+                res.body.should.have.property('name');
+                res.body.should.have.property('comment');
+            });
+    });
+});
